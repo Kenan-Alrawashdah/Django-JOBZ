233e9a761aee421a198629dd1dfebbe76b39c220
KenanAl <kenan.rawashdah@gmail.com>
2022-07-02 23:38:24 +0300

    update model from word to jop and create views and file urls to jop

---
+++ jop/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Jop, Category
+
+admin.site.register(Jop)
+admin.site.register(Category)
--- jop/apps.py
+++ jop/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class WordConfig(AppConfig):
+class JopConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'word'
+    name = 'jop'
