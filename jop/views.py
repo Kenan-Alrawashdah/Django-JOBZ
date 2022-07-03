fe521cbab19c59c12b5f9c57b0fdb00eff89bd16
KenanAl <kenan.rawashdah@gmail.com>
2022-07-03 12:00:02 +0300

    add implement views and add tow page html to test

--- jop/views.py
+++ jop/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
+from .models import Jop
 # Create your views here.
 
 
 def jop_list(request):
-    pass
+    jop_list = Jop.objects.all()
+    context = {'jops': jop_list}
+    return render(request, 'jop/jop_list.html', context)
 
 
 def jop_detail(request, id):
-    pass
+    jop_detail = Jop.objects.get(id = id)
+    context = {'jop' : jop_detail}
+    return render(request, 'jop/jop_detail.html', context)
