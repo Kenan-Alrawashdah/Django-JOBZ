2a1b61525b71a90e0e804434957e11bc80ba9f06
KenanAl <kenan.rawashdah@gmail.com>
2022-07-17 11:27:33 +0300

    add pagination

--- jop/urls.py
+++ jop/urls.py
@@ -3,7 +3,8 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'jop'
 urlpatterns = [
     path('', views.jop_list),
-    path('<int:id>', views.jop_detail),
+    path('<int:id>', views.jop_detail, name='jop_detail'),
 ]
--- jop/views.py
+++ jop/views.py
@@ -1,15 +1,29 @@
+from distutils.log import debug
 from django.shortcuts import render
-from .models import Jop
+from .models import Category, Jop
+from django.core.paginator import Paginator
+
 # Create your views here.
 
+def get_categories(request):
+    get_categories = Category.objects.all()
+    context = {'categories': get_categories}
+    return render(request, 'jop/jop_list.html', context)
 
 def jop_list(request):
     jop_list = Jop.objects.all()
-    context = {'jops': jop_list}
+
+    paginator = Paginator(jop_list, 2) # Show 25 contacts per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'jops': page_obj}
     return render(request, 'jop/jop_list.html', context)
 
 
 def jop_detail(request, id):
-    jop_detail = Jop.objects.get(id = id)
-    context = {'jop' : jop_detail}
-    return render(request, 'jop/job_detail.html', context)
+     jop_detail = Jop.objects.get(id = id)
+     context = {'jop' : jop_detail}
+     return render(request, 'jop/job_detail.html', context)
+
+
+
