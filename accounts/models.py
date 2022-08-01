9f908f4cf052fbd43baf441ac042757bbfeb5a97
KenanAl <kenan.rawashdah@gmail.com>
2022-08-01 19:29:34 +0300

    add page signup and profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,27 @@
+from email.mime import image
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User ,on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=10)
+    image = models.FileField(upload_to='profile/')
+    city = models.ForeignKey('City', related_name='user_city', on_delete=models.CASCADE, blank=True, null=True)
+    
+    def __str__(self):
+        return str(self.user)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+class City(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__ (self):
+        return str(self.name)
--- jop/urls.py
+++ jop/urls.py
@@ -1,9 +1,9 @@
 
-
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 app_name = 'jop'
+
 urlpatterns = [
     path('', views.jop_list, name='jop_list'),
     path('<int:id>', views.jop_detail, name='jop_detail'),
