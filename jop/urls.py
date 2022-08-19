8324cf0255895f397ab17489ef594a30d995ae7d
KenanAl <kenan.rawashdah@gmail.com>
2022-08-19 22:09:04 +0300

    add methods update_job and delete_job

--- jop/urls.py
+++ jop/urls.py
@@ -7,7 +7,11 @@ app_name = 'jop'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:id>/jop_list', views.jop_list, name='jop_list'),
+    path('my_job', views.my_job, name='my_job'),
     path('<int:id>', views.jop_detail, name='jop_detail'),
     path('add', views.jop_add, name='jop_add'),
     path('<str:id>/apply', views.jop_apply, name='jop_apply'),
+    path('<str:id>/edit', views.update_job, name='jop_edit'),
+    path('<str:id>/delete', views.delete_job, name='job_delete'),
+
 ]
--- jop/views.py
+++ jop/views.py
@@ -12,14 +12,18 @@ from django.contrib.auth.decorators import login_required
 
 def index(request):
     categories = Category.objects.all()
-    context = {'categories':categories}
+    jobs = Jop.objects.all().order_by('-published_at')[:5]
+    context = {'categories':categories, 'jobs':jobs}
     return render(request, 'jop/index.html', context)
 
 
 def jop_list(request, id):
-    categories_id = Category.objects.get(id=id)
-    jop_list = Jop.objects.filter(category=categories_id)
-    #jop_list = Jop.objects.all()
+    if id == "0":
+        jop_list = Jop.objects.all()
+    else :
+        categories_id = Category.objects.get(id=id)
+        jop_list = Jop.objects.filter(category=categories_id)
+        
     categories = Category.objects.all()
     filter_list = JopFilter(request.GET, queryset= jop_list)
     jop_list = filter_list.qs
@@ -44,7 +48,7 @@ def jop_add(request):
             myform = form.save(commit=False)
             myform.user = request.user
             myform.save()
-            return redirect('jops:index')
+            return redirect('jops:my_job')
 
     else:
         form = JopForm()
@@ -67,8 +71,46 @@ def jop_apply(request, id):
     else:
         form = ApplyForm()
     
-    context = {'form': form}
+    context = {'form': form, 'id':id}
     return render(request, 'jop/jop_apply.html', context)         
 
+
+
+def my_job(request):
+    user = request.user
+    jop_list = Jop.objects.filter(user=user)
+    categories = Category.objects.all()
+    filter_list = JopFilter(request.GET, queryset= jop_list)
+    jop_list = filter_list.qs
+    paginator = Paginator(jop_list, 3) # Show 25 contacts per page.
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {'jops': page_obj, 'categories': categories, 'filter': filter_list}
+    return render(request, 'jop/my_job.html', context)
+
+
+def update_job(request, id):
+    job = Jop.objects.get(id=id)
+    
+    if request.method == "POST":
+        jobform = JopForm(request.POST, request.FILES, instance=job)
+        if jobform.is_valid:
+            myjobform = jobform.save(commit=False)
+            myjobform.user = request.user
+            myjobform.save()
+            return redirect('/jops/my_job')
+    else :
+        jobform = JopForm(instance=job)
+    return render(request, 'jop/job_edit.html',{'jobform': jobform})    
+
+
+def delete_job(request, id):
+    jop = Jop.objects.get(id= id)
+
+    if request.method == "POST":
+        jop.delete()
+        return redirect('/jops/my_job')
+    
+    return render(request, 'jop/job_delete.html', {'jop': jop})
 
 
