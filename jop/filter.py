61778827a07920030c4aa0a0cd8f310b27d4f917
KenanAl <kenan.rawashdah@gmail.com>
2022-08-19 22:15:42 +0300

    fix filter company to contains search

--- jop/filter.py
+++ jop/filter.py
@@ -6,7 +6,10 @@ from .models import Jop
 class JopFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr='icontains')
     description = django_filters.CharFilter(lookup_expr='icontains')
+    company = django_filters.CharFilter(lookup_expr='icontains')
     class Meta:
         model = Jop
         fields = '__all__'
         exclude = ['user', 'price', 'image', 'vacancy', 'salary', 'experienc', 'published_at']
+
+
